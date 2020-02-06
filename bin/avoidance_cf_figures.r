e985b57cd717f5dec0935a2cc04e9a556d8819fd
Akhil <akhilrao72@gmail.com>
2020-02-06 12:44:38 -0500

    updated counterfactual simulations

---
+++ bin/avoidance_cf_figures.r
@@ -0,0 +1,134 @@
+##### Script to generate figures for collision avoidance analysis
+###
+# Script flow:
+# 1. Read in counterfactuals with collision avoidance scenario
+# 2. Generate individual figures
+# 3. Generate figure panels
+
+#############################################################################
+# 1.  Read in alternate discount rate results, make additional outcome variables
+#############################################################################
+
+OA_OPT_estimated <- read.csv("../data/2006_7_starts_remfrac_0_remstart_2029_main_simulation.csv")
+OA_OPT_avoid <- read.csv("../data/counterfactuals/collision_avoidance/2006_cf_avoidance_aSS_-Inf_aSD_-18.2_simulation.csv")
+
+OA_OPT_estimated <- calc_tax_path(OA_OPT_estimated)
+OA_OPT_avoid <- calc_tax_path(OA_OPT_avoid)
+
+setwd("../data/counterfactuals/collision_avoidance/")
+
+#############################################################################
+# 2.  Generate individual figures
+#############################################################################
+
+selection <- c("year","start_year","start_time.opt","launches.oa","satellites.oa","debris.oa","collision_rate.oa",
+											"launches.opt","satellites.opt","debris.opt","collision_rate.opt",
+											"NPVPoA","opt_tax_path")
+OA_OPT_est.small <- OA_OPT_estimated[,selection]
+OA_OPT_avo.small <- OA_OPT_avoid[,selection]
+
+OA_OPT_est.small <- ddply(OA_OPT_est.small, .(start_year), transform, shifted_tax=c(opt_tax_path[-1],0))
+OA_OPT_est.small <- OA_OPT_est.small[-which(OA_OPT_est.small$year==2041),]
+
+OA_OPT_avo.small <- ddply(OA_OPT_avo.small, .(start_year), transform, shifted_tax=c(opt_tax_path[-1],0))
+OA_OPT_avo.small <- OA_OPT_avo.small[-which(OA_OPT_avo.small$year==2041),]
+
+OA_OPT <- merge(OA_OPT_est.small,OA_OPT_avo.small,by=c("year","start_year","start_time.opt"), suffix=c(".est",".avo") )
+
+OA_fit_color <- "red"
+OPT_fit_color <- "blue"
+OA_OPT_fit_size <- 0.8
+data_size <- 1
+
+OA_OPT_base_proj <- ggplot(data=OA_OPT[which(OA_OPT$start_time.opt==14),],aes(x=year))
+
+(OA_OPT_launch_proj <- OA_OPT_base_proj + 
+	geom_line(aes(y=launches.opt.est),linetype="dashed",color=OPT_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=launches.oa.est),linetype="dashed",color=OA_fit_color,size=OA_OPT_fit_size) +	
+	geom_line(aes(y=launches.opt.avo),color=OPT_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=launches.oa.avo),color=OA_fit_color,size=OA_OPT_fit_size) +	
+	ylab("Satellites launched") + xlab("") + theme_bw() +
+	ggtitle("Launches")	+
+				theme(text=element_text(family="Helvetica",size=20),
+					axis.text.x=element_text(family="Helvetica",size=20),
+					axis.text.y=element_text(family="Helvetica",size=20),
+					plot.title=element_text(family="Helvetica",size=20),
+					legend.text=element_text(family="Helvetica",size=20) ) )
+
+(OA_OPT_sat_proj <- OA_OPT_base_proj + 
+	geom_line(aes(y=satellites.opt.est),linetype="dashed",color=OPT_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=satellites.oa.est),linetype="dashed",color=OA_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=satellites.opt.avo),color=OPT_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=satellites.oa.avo),color=OA_fit_color,size=OA_OPT_fit_size) +
+	#geom_vline(xintercept=2015,size=1,linetype="dashed") +
+	ggtitle("Satellites") +
+	ylab("LEO satellite stock") + xlab("") + theme_bw() +
+	theme(text=element_text(family="Helvetica",size=20),
+		axis.text.x=element_text(family="Helvetica",size=20),
+		axis.text.y=element_text(family="Helvetica",size=20),
+		plot.title=element_text(family="Helvetica",size=20),
+		legend.text=element_text(family="Helvetica",size=20) ))
+
+(OA_OPT_deb_proj <- OA_OPT_base_proj + 
+	geom_line(aes(y=debris.opt.est),linetype="dashed",color=OPT_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=debris.oa.est),linetype="dashed",color=OA_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=debris.opt.avo),color=OPT_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=debris.oa.avo),color=OA_fit_color,size=OA_OPT_fit_size) +
+	#geom_vline(xintercept=2015,size=1,linetype="dashed") +
+	ggtitle("Debris") +
+	ylab("LEO debris stock") + xlab("Year") + theme_bw()	+
+				theme(text=element_text(family="Helvetica",size=20),
+					axis.text.x=element_text(family="Helvetica",size=20),
+					axis.text.y=element_text(family="Helvetica",size=20),
+					plot.title=element_text(family="Helvetica",size=20),
+					legend.text=element_text(family="Helvetica",size=20) ))
+
+(OA_OPT_risk_proj <- OA_OPT_base_proj + 
+	geom_line(aes(y=collision_rate.opt.est/satellites.opt.est),linetype="dashed",color=OPT_fit_color, size=OA_OPT_fit_size) +
+	geom_line(aes(y=collision_rate.oa.est/satellites.oa.est),linetype="dashed",color=OA_fit_color,size=OA_OPT_fit_size) +
+	geom_line(aes(y=collision_rate.opt.avo/satellites.opt.avo),color=OPT_fit_color, size=OA_OPT_fit_size) +
+	geom_line(aes(y=collision_rate.oa.avo/satellites.oa.avo),color=OA_fit_color,size=OA_OPT_fit_size) +
+	#geom_vline(xintercept=2015,size=1,linetype="dashed") +
+	ggtitle("Collision probability") +
+	ylab("LEO collision risk") + xlab("Year") + theme_bw()	+
+	theme(text=element_text(family="Helvetica",size=20),
+		axis.text.x=element_text(family="Helvetica",size=20),
+		axis.text.y=element_text(family="Helvetica",size=20),
+		plot.title=element_text(family="Helvetica",size=20),
+		legend.text=element_text(family="Helvetica",size=20) ) )
+
+(opt_tax_path <- OA_OPT_base_proj + 
+	geom_line(aes(y=shifted_tax.est),linetype="dashed",size=data_size) + theme_bw() +
+	geom_line(aes(y=shifted_tax.avo),size=data_size) + theme_bw() +
+	labs(color="") +
+	scale_y_continuous(name="Optimal OUF (nominal $/sat)", labels = scales::comma) +
+	xlab("Year") +
+	ggtitle("Optimal OUF path") +
+	expand_limits(y=0) +
+	theme(text=element_text(family="Helvetica",size=20),
+		axis.text.x=element_text(family="Helvetica",size=20),
+		axis.text.y=element_text(family="Helvetica",size=20),
+		plot.title=element_text(family="Helvetica",size=20),
+		legend.text=element_text(family="Helvetica",size=20) ) )
+
+(NPVPoA_path <- OA_OPT_base_proj + 
+	geom_line(aes(y=NPVPoA.est),linetype="dashed",size=data_size) + theme_bw() +
+	geom_line(aes(y=NPVPoA.avo),size=data_size) + theme_bw() +
+	labs(color="") +
+	scale_y_continuous(name="Ratio of optimal NPV to BAU NPV", labels = scales::comma) +
+	xlab("Year") +
+	ggtitle("NPV gains from optimal management") +
+	expand_limits(y=0) +
+	theme(text=element_text(family="Helvetica",size=20),
+		axis.text.x=element_text(family="Helvetica",size=20),
+		axis.text.y=element_text(family="Helvetica",size=20),
+		plot.title=element_text(family="Helvetica",size=20),
+		legend.text=element_text(family="Helvetica",size=20) ) )
+
+#############################################################################
+# 3.  Generate figure panel
+#############################################################################
+
+png(width=900,height=600,filename=paste0("../../../images/collision_avoidance_counterfactual.png"))
+	plot_grid(OA_OPT_launch_proj,OA_OPT_sat_proj,OA_OPT_risk_proj,OA_OPT_deb_proj,opt_tax_path,NPVPoA_path,align="v",axis="1",labels=c("a","b","c","d","e","f"),nrow=2,ncol=3,rel_widths=c(1/3,1/3,1/3),label_size=25)
+dev.off()
--- bin/discount_cf_figures.r
+++ bin/discount_cf_figures.r
@@ -49,3 +49,28 @@ data_size <- 1
 		legend.text=element_text(family="Helvetica",size=20) ) ) 
 
 
+boa_fin_dfrm.discount_cf <- OA_OPT_tax_shift[which(OA_OPT_tax_shift$year==2040),c("NPVPoA","discount_rate","year")]
+row.names(boa_fin_dfrm.discount_cf) <- NULL
+
+aes(fill=as.factor(NPVPoA)),
+
+(boa_plot_pc.discount_cf <- ggplot(data=boa_fin_dfrm.discount_cf,aes(discount_rate,NPVPoA)) +
+				geom_bar(position="dodge", stat="identity") +
+				geom_hline(yintercept = 4) +
+				geom_hline(,linetype="dashed",alpha=0.5,yintercept = 1) +
+				ylab("Ratio of optimal NPV to BAU NPV in 2040") +
+				xlab("Planner's discount rate") +
+				theme_bw() +
+				scale_y_continuous(minor_breaks=seq(0,40,5),breaks=c(0,1,4,10,20,30)) +
+				scale_color_viridis(discrete=FALSE) +
+				guides(fill=FALSE) +
+				theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm")) +
+				theme(text=element_text(family="Helvetica",size=20),
+					axis.text.x=element_text(family="Helvetica",size=15),
+					axis.text.y=element_text(family="Helvetica",size=15),
+					plot.title=element_text(family="Helvetica",size=15),
+					legend.text=element_text(family="Helvetica",size=15) ) )
+
+png(width=900,height=600,filename=paste0("../../../images/discount_rate_sensitivity.png"))
+	plot_grid(boa_plot_pc.discount_cf,opt_tax_path,align="v",axis="1",labels=c("a","b"),nrow=1,rel_widths=c(1/2,1/2),label_size=25)
+dev.off()
